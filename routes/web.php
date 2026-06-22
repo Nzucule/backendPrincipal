a8193ccfc056c6dfee198870a1385fefd55d27fa
Abilio <abiliodanieln@gmail.com>
2026-06-22 19:20:00 +0200

    remove secrets

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Mail;
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
-Route::get('/teste-email', function () {
+Route::get('/teste-brevo-api', function () {
 
-    try {
-        Mail::raw('Teste de envio do App Pest Protect', function ($message) {
-            $message->to('abiliodanieln@gmail.com')
-                    ->subject('Teste Brevo Laravel');
-        });
+    $response = Http::withHeaders([
+        'api-key' => env('BREVO_API_KEY'),
+    ])->post('https://api.brevo.com/v3/smtp/email', [
+        "sender" => [
+            "name" => "App Pest Protect",
+            "email" => env('BREVO_SENDER_EMAIL', 'abiliodanieln@gmail.com')
+        ],
+        "to" => [
+            ["email" => "abiliodanieln@gmail.com"]
+        ],
+        "subject" => "Teste Brevo API",
+        "htmlContent" => "<p>Teste email via API</p>"
+    ]);
 
-        return 'EMAIL ENVIADO COM SUCESSO';
-
-    } catch (\Exception $e) {
-        return 'ERRO: ' . $e->getMessage();
-    }
+    return $response->json();
 });
